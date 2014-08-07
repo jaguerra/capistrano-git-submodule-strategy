d7ebbfb97aeb41ff75e7c6b189b05e83de860139
Jose Antonio Guerra <jaguerra@icti.es>
2014-08-07 13:39:27 +0200

    [WIP] Deploying with submodules breaks on rails project
    The "release" gets called twice so the second run of
    "git checkout" breaks the build.
    
    Cloning recursively is now done on a temp folder and
    then cleaned up and copied to release.
    
    Other strategy methods than "release" have been cleaned
    up as they are included from the base anyway.
    
    Still need to add tests though.

--- lib/capistrano/git-submodule-strategy.rb
+++ lib/capistrano/git-submodule-strategy.rb
@@ -5,38 +5,15 @@ class Capistrano::Git
     # do all the things a normal capistrano git session would do
     include Capistrano::Git::DefaultStrategy
 
-    def test
-      test! " [ -f #{repo_path}/HEAD ] "
-    end
-
-    def check
-      unless test!(:git, :'ls-remote', repo_url)
-        context.error "Repo `#{repo_url}` does not exists"
-        return false
-      end
-
-      if context.capture(:git, :'ls-remote', repo_url).split("\n").select{ |i| i.include?("refs/heads/#{fetch(:branch)}") }.empty?
-        context.error "Branch `#{fetch(:branch)}` not found in repo `#{repo_url}`"
-        return false
-      end
-
-      true
-    end
-
-    def clone
-      git :clone, '--mirror', repo_url, repo_path
-    end
-
-    def update
-      git :remote, :update
-    end
-
     # put the working tree in a release-branch,
     # make sure the submodules are up-to-date
     # and copy everything to the release path
     def release
-      git :clone, '--depth=1', '--recursive', '-b', fetch(:branch), "file://#{repo_path}", release_path
-      context.execute("find #{release_path} -name '.git*' | xargs -I {} rm -rfv {}")
+      temp_path = File.join(fetch(:tmp_dir), 'repo')
+      context.execute("rm -Rf #{temp_path}")
+      git :clone, '--depth=1', '--recursive', '-b', fetch(:branch), "file://#{repo_path}", temp_path
+      context.execute("find #{temp_path} -name '.git*' | xargs -I {} rm -rfv {}")
+      context.execute("cp -R #{temp_path}/. #{release_path}/")
     end
   end
 end
